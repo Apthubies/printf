8d4204db4f00c3bf2dc59c03e33a258823a13cc3
Bobbyxmb <emmanuelwalter715@gmail.com>
2023-10-14 18:05:24 -0700

    updated _printf.c file

--- _printf.c
+++ _printf.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
-void print_buffer(char buffer[], int *buff_ind);
+void print_buffer(char buffer[], int *buff_loc);
 
 /**
  * _printf - Printf function
  * @format: format.
- * Return: Printed chars.
+ * Return: generated chars.
  */
 int _printf(const char *format, ...)
 {
-	int i, printed = 0, printed_chars = 0;
-	int flags, width, precision, size, buff_ind = 0;
+	int p, generated = 0, generated_chars = 0;
+	int isDataLoaded, breadth, resolution, magnitude, buff_loc = 0;
 	va_list list;
 	char buffer[BUFF_SIZE];
 
@@ -19,49 +19,49 @@ int _printf(const char *format, ...)
 
 	va_start(list, format);
 
-	for (i = 0; format && format[i] != '\0'; i++)
+	for (p = 0; format && format[p] != '\0'; p++)
 	{
-		if (format[i] != '%')
+		if (format[p] != '%')
 		{
-			buffer[buff_ind++] = format[i];
-			if (buff_ind == BUFF_SIZE)
-				print_buffer(buffer, &buff_ind);
-			/* write(1, &format[i], 1);*/
-			printed_chars++;
+			buffer[buff_loc++] = format[p];
+			if (buff_loc == BUFF_SIZE)
+				print_buffer(buffer, &buff_loc);
+			/* write(1, &format[p], 1);*/
+			generated_chars++;
 		}
 		else
 		{
-			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &i);
-			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, list);
-			size = get_size(format, &i);
-			++i;
-			printed = handle_print(format, &i, list, buffer,
-				flags, width, precision, size);
+			print_buffer(buffer, &buff_loc);
+			isDataLoaded = get_flags(format, &p);
+			breadth = get_width(format, &p, list);
+			resolution = get_precision(format, &p, list);
+			magnitude = get_size(format, &p);
+			++p;
+			generated = handle_print(format, &p, list, buffer,
+				isDataLoaded, breadth, resolution, magnitude);
 			if (printed == -1)
 				return (-1);
-			printed_chars += printed;
+			generated_chars += generated;
 		}
 	}
 
-	print_buffer(buffer, &buff_ind);
+	print_buffer(buffer, &buff_loc);
 
 	va_end(list);
 
-	return (printed_chars);
+	return (generated_chars);
 }
 
 /**
  * print_buffer - Prints the contents of the buffer if it exist
  * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
+ * @buff_loc: Location at which to add next char, represents the length.
  */
-void print_buffer(char buffer[], int *buff_ind)
+void print_buffer(char buffer[], int *buff_loc)
 {
-	if (*buff_ind > 0)
-		write(1, &buffer[0], *buff_ind);
+	if (*buff_loc > 0)
+		write(1, &buffer[0], *buff_loc);
 
-	*buff_ind = 0;
+	*buff_loc = 0;
 }
 
